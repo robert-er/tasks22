f366e66a22a273e8c37dce00540b282319c3ce6f
robert-er <robert.roza.pl@gmail.com>
2020-11-18 13:33:28 +0100

    setupTrelloBoards

--- src/test/java/com/crud/tasks/mapper/TrelloMapperTest.java
+++ src/test/java/com/crud/tasks/mapper/TrelloMapperTest.java
@@ -27,35 +27,7 @@ class TrelloMapperTest {
     @Test
     void mapToBoards() {
         //Given
-        List<TrelloBoardDto> trelloBoardsDto = new ArrayList<>();
-
-        TrelloBoardDto trelloBoardDto1 = new TrelloBoardDto("ID-BOARD-1", "Board1", new ArrayList<>());
-        TrelloListDto trelloListDto1 = new TrelloListDto("ID-LIST-1", "List1", false);
-        TrelloListDto trelloListDto2 = new TrelloListDto("ID-LIST-2", "List2", false);
-        TrelloListDto trelloListDto3 = new TrelloListDto("ID-LIST-3", "List3", false);
-        trelloBoardDto1.getLists().add(trelloListDto1);
-        trelloBoardDto1.getLists().add(trelloListDto2);
-        trelloBoardDto1.getLists().add(trelloListDto3);
-
-        TrelloBoardDto trelloBoardDto2 = new TrelloBoardDto("ID-BOARD-2", "Board2", new ArrayList<>());
-        TrelloListDto trelloListDto4 = new TrelloListDto("ID-LIST-4", "List4", false);
-        TrelloListDto trelloListDto5 = new TrelloListDto("ID-LIST-5", "List5", false);
-        TrelloListDto trelloListDto6 = new TrelloListDto("ID-LIST-6", "List6", false);
-        trelloBoardDto2.getLists().add(trelloListDto4);
-        trelloBoardDto2.getLists().add(trelloListDto5);
-        trelloBoardDto2.getLists().add(trelloListDto6);
-
-        TrelloBoardDto trelloBoardDto3 = new TrelloBoardDto("ID-BOARD-3", "Board3", new ArrayList<>());
-        TrelloListDto trelloListDto7 = new TrelloListDto("ID-LIST-7", "List7", false);
-        TrelloListDto trelloListDto8 = new TrelloListDto("ID-LIST-8", "List8", false);
-        TrelloListDto trelloListDto9 = new TrelloListDto("ID-LIST-9", "List9", false);
-        trelloBoardDto3.getLists().add(trelloListDto7);
-        trelloBoardDto3.getLists().add(trelloListDto8);
-        trelloBoardDto3.getLists().add(trelloListDto9);
-
-        trelloBoardsDto.add(trelloBoardDto1);
-        trelloBoardsDto.add(trelloBoardDto2);
-        trelloBoardsDto.add(trelloBoardDto3);
+        List<TrelloBoardDto> trelloBoardsDto = setupTrelloBoardsDto();
 
         //When
         List<TrelloBoard> trelloBoards = trelloMapper.mapToBoards(trelloBoardsDto);
@@ -68,35 +40,7 @@ class TrelloMapperTest {
     @Test
     void mapToBoardsDto() {
         //Given
-        List<TrelloBoard> trelloBoards = new ArrayList<>();
-
-        TrelloBoard trelloBoard1 = new TrelloBoard("ID-BOARD-1", "Board1", new ArrayList<>());
-        TrelloList trelloList1 = new TrelloList("ID-LIST-1", "List1", false);
-        TrelloList trelloList2 = new TrelloList("ID-LIST-2", "List2", false);
-        TrelloList trelloList3 = new TrelloList("ID-LIST-3", "List3", false);
-        trelloBoard1.getLists().add(trelloList1);
-        trelloBoard1.getLists().add(trelloList2);
-        trelloBoard1.getLists().add(trelloList3);
-
-        TrelloBoard trelloBoard2 = new TrelloBoard("ID-BOARD-2", "Board2", new ArrayList<>());
-        TrelloList trelloList4 = new TrelloList("ID-LIST-4", "List4", false);
-        TrelloList trelloList5 = new TrelloList("ID-LIST-5", "List5", false);
-        TrelloList trelloList6 = new TrelloList("ID-LIST-6", "List6", false);
-        trelloBoard2.getLists().add(trelloList4);
-        trelloBoard2.getLists().add(trelloList5);
-        trelloBoard2.getLists().add(trelloList6);
-
-        TrelloBoard trelloBoard3 = new TrelloBoard("ID-BOARD-3", "Board3", new ArrayList<>());
-        TrelloList trelloList7 = new TrelloList("ID-LIST-7", "List7", false);
-        TrelloList trelloList8 = new TrelloList("ID-LIST-8", "List8", false);
-        TrelloList trelloList9 = new TrelloList("ID-LIST-9", "List9", false);
-        trelloBoard3.getLists().add(trelloList7);
-        trelloBoard3.getLists().add(trelloList8);
-        trelloBoard3.getLists().add(trelloList9);
-
-        trelloBoards.add(trelloBoard1);
-        trelloBoards.add(trelloBoard2);
-        trelloBoards.add(trelloBoard3);
+        List<TrelloBoard> trelloBoards = setupTrelloBoards();
 
         //When
         List<TrelloBoardDto> trelloBoardsDto = trelloMapper.mapToBoardsDto(trelloBoards);
@@ -140,5 +84,72 @@ class TrelloMapperTest {
         assertEquals(trelloCard.getDescription(), trelloCardDto.getDescription());
         assertEquals(trelloCard.getPos(), trelloCardDto.getPos());
         assertEquals(trelloCard.getListId(), trelloCardDto.getListId());
+    }
+
+    private List<TrelloBoardDto> setupTrelloBoardsDto() {
+        List<TrelloBoardDto> trelloBoardsDto = new ArrayList<>();
+        TrelloBoardDto trelloBoardDto1 = new TrelloBoardDto("ID-BOARD-1", "Board1", new ArrayList<>());
+        TrelloListDto trelloListDto1 = new TrelloListDto("ID-LIST-1", "List1", false);
+        TrelloListDto trelloListDto2 = new TrelloListDto("ID-LIST-2", "List2", false);
+        TrelloListDto trelloListDto3 = new TrelloListDto("ID-LIST-3", "List3", false);
+        trelloBoardDto1.getLists().add(trelloListDto1);
+        trelloBoardDto1.getLists().add(trelloListDto2);
+        trelloBoardDto1.getLists().add(trelloListDto3);
+
+        TrelloBoardDto trelloBoardDto2 = new TrelloBoardDto("ID-BOARD-2", "Board2", new ArrayList<>());
+        TrelloListDto trelloListDto4 = new TrelloListDto("ID-LIST-4", "List4", false);
+        TrelloListDto trelloListDto5 = new TrelloListDto("ID-LIST-5", "List5", false);
+        TrelloListDto trelloListDto6 = new TrelloListDto("ID-LIST-6", "List6", false);
+        trelloBoardDto2.getLists().add(trelloListDto4);
+        trelloBoardDto2.getLists().add(trelloListDto5);
+        trelloBoardDto2.getLists().add(trelloListDto6);
+
+        TrelloBoardDto trelloBoardDto3 = new TrelloBoardDto("ID-BOARD-3", "Board3", new ArrayList<>());
+        TrelloListDto trelloListDto7 = new TrelloListDto("ID-LIST-7", "List7", false);
+        TrelloListDto trelloListDto8 = new TrelloListDto("ID-LIST-8", "List8", false);
+        TrelloListDto trelloListDto9 = new TrelloListDto("ID-LIST-9", "List9", false);
+        trelloBoardDto3.getLists().add(trelloListDto7);
+        trelloBoardDto3.getLists().add(trelloListDto8);
+        trelloBoardDto3.getLists().add(trelloListDto9);
+
+        trelloBoardsDto.add(trelloBoardDto1);
+        trelloBoardsDto.add(trelloBoardDto2);
+        trelloBoardsDto.add(trelloBoardDto3);
+
+        return trelloBoardsDto;
+    }
+
+    private List<TrelloBoard> setupTrelloBoards() {
+        List<TrelloBoard> trelloBoards = new ArrayList<>();
+
+        TrelloBoard trelloBoard1 = new TrelloBoard("ID-BOARD-1", "Board1", new ArrayList<>());
+        TrelloList trelloList1 = new TrelloList("ID-LIST-1", "List1", false);
+        TrelloList trelloList2 = new TrelloList("ID-LIST-2", "List2", false);
+        TrelloList trelloList3 = new TrelloList("ID-LIST-3", "List3", false);
+        trelloBoard1.getLists().add(trelloList1);
+        trelloBoard1.getLists().add(trelloList2);
+        trelloBoard1.getLists().add(trelloList3);
+
+        TrelloBoard trelloBoard2 = new TrelloBoard("ID-BOARD-2", "Board2", new ArrayList<>());
+        TrelloList trelloList4 = new TrelloList("ID-LIST-4", "List4", false);
+        TrelloList trelloList5 = new TrelloList("ID-LIST-5", "List5", false);
+        TrelloList trelloList6 = new TrelloList("ID-LIST-6", "List6", false);
+        trelloBoard2.getLists().add(trelloList4);
+        trelloBoard2.getLists().add(trelloList5);
+        trelloBoard2.getLists().add(trelloList6);
+
+        TrelloBoard trelloBoard3 = new TrelloBoard("ID-BOARD-3", "Board3", new ArrayList<>());
+        TrelloList trelloList7 = new TrelloList("ID-LIST-7", "List7", false);
+        TrelloList trelloList8 = new TrelloList("ID-LIST-8", "List8", false);
+        TrelloList trelloList9 = new TrelloList("ID-LIST-9", "List9", false);
+        trelloBoard3.getLists().add(trelloList7);
+        trelloBoard3.getLists().add(trelloList8);
+        trelloBoard3.getLists().add(trelloList9);
+
+        trelloBoards.add(trelloBoard1);
+        trelloBoards.add(trelloBoard2);
+        trelloBoards.add(trelloBoard3);
+
+        return trelloBoards;
     }
 }
