0f6b906f823bc0cf5e7b3f922bf73061707dc77c
robert-er <robert.roza.pl@gmail.com>
2020-12-09 15:16:02 +0100

    zadanie32.2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,8 +2,6 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class TasksApplication  {
@@ -11,10 +9,4 @@ public class TasksApplication  {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
-
-	//for Tomcat
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
-//		return applicationBuilder.sources(TasksApplication.class);
-//	}
 }
